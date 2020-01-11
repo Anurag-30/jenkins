80a63dbcc236c944ec5d6d3ef169f3ce275658e4
Anuragp <anuragp@Anuragps-MacBook-Pro.local>
2020-01-11 09:31:00 +0530

    change in shell script

--- jenkins-setup.sh
+++ jenkins-setup.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 kubectl create ns ci-cd
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 helm install jenkins stable/jenkins -f jenkins-values.yml --namespace ci-cd
 
