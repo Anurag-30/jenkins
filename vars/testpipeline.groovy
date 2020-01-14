2572b612caf49ae7202bd9385fdf8b2cfe962c55
Anuragp <anuragp@Anuragps-MacBook-Pro.local>
2020-01-14 16:35:32 +0530

    --- Auto-Git Commit ---

--- vars/testpipeline.groovy
+++ vars/testpipeline.groovy
@@ -4,11 +4,6 @@ def call(Map pipelineParams) {
     pipeline {
         agent any
         stages {
-            stage('checkout git') {
-                steps {
-                    git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl
-                }
-            }
 
             stage('build') {
                 steps {
