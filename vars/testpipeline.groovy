6dd7e376a57dd118dc432ff3bf8a128d9ecd5916
Anuragp <anuragp@Anuragps-MacBook-Pro.local>
2020-01-14 16:48:03 +0530

    --- Auto-Git Commit ---

--- vars/testpipeline.groovy
+++ vars/testpipeline.groovy
@@ -10,24 +10,8 @@ def call(Map pipelineParams) {
                     sh 'pwd'
                 }
             }
-
-
-            stage('deploy staging'){
-                steps {
-                    deploy(pipelineParams.stagingServer, pipelineParams.serverPort)
-                }
-            }
-
-            stage('deploy production'){
-                steps {
-                    deploy(pipelineParams.productionServer, pipelineParams.serverPort)
-                }
-            }
         }
-        post {
-            failure {
-                mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
-            }
+        
         }
     }
 }
