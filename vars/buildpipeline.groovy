dc7153cf6dd8c92cf289008b2b6e876c2851e3a3
Anuragp <anuragp@Anuragps-MacBook-Pro.local>
2020-01-14 12:20:16 +0530

    shared lib

--- vars/buildpipeline.groovy
+++ vars/buildpipeline.groovy
@@ -1,15 +1,5 @@
 def call(Map pipelineParams) {
-podTemplate(label: label, containers: [
-  containerTemplate(name: 'gradle', image: 'gradle:latest', command: 'cat', ttyEnabled: true),
-  containerTemplate(name: 'git', image: 'alpine/git:latest', command: 'cat', ttyEnabled: true),
-  containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true),
-  containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.8.8', command: 'cat', ttyEnabled: true),
-  containerTemplate(name: 'helm', image: 'lachlanevenson/k8s-helm:latest', command: 'cat', ttyEnabled: true)
-],
-volumes: [
-  hostPathVolume(mountPath: '/home/gradle/.gradle', hostPath: '/tmp/jenkins/.gradle'),
-  hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock')
-])
+
     pipeline {
         agent any
         stages {
