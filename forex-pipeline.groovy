1746b37db97582c787388ea0eaef1251e96196e2
Anuragp <anuragp@Anuragps-MacBook-Pro.local>
2020-01-28 16:12:34 +0530

    --- Auto-Git Commit ---

--- forex-pipeline.groovy
+++ forex-pipeline.groovy
@@ -30,8 +30,8 @@ spec:
                 steps {
                     container(''){
                     sh '''
+                   
                     gradle build docker --stacktrace
-                    docker login artifactory.dev.maximus.axisb.com/artifactory/docker -u=${ARTIFACTORY_USER} -p=${ARTIFACTORY_PASSWORD}
                     docker push artifactory.dev.maximus.axisb.com/docker/${JENKINS_PIPELINE_NAME}:latest
                     docker tag artifactory.dev.maximus.axisb.com/docker/${JENKINS_PIPELINE_NAME}:latest artifactory.dev.maximus.axisb.com/docker/${JENKINS_PIPELINE_NAME}:${JENKINS_PIPELINE_LABEL}
                     docker push artifactory.dev.maximus.axisb.com/docker/${JENKINS_PIPELINE_NAME}:${JENKINS_PIPELINE_LABEL}
