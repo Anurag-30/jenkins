e9ebd42b9606e180a383b71ecf939abd6fad9f17
Anuragp <anuragp@Anuragps-MacBook-Pro.local>
2020-01-28 14:54:43 +0530

    --- Auto-Git Commit ---

---
+++ forex-pipeline.groovy
@@ -0,0 +1,54 @@
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent {
+               kubernetes {
+      yaml """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    some-label: some-label-value
+spec:
+  containers:
+  - name: gradle
+    image: gradle:latest
+    command:
+    - cat
+    tty: true
+/*  - name: busybox
+    image: busybox
+    command:
+    - cat
+    tty: true */
+"""
+    }
+             
+ 
+    }
+
+
+
+        stages {
+
+            stage('build') {
+                steps {
+                    container(''){
+                    sh '''
+                    gradle build docker --stacktrace
+                    '''
+                }
+            }
+        }
+
+            stage('t') {
+                steps {
+                    
+              build job: 'deploy-job'
+                
+            }
+        }
+        
+    }
+ }
+} 
