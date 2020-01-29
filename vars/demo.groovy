08c46a4e94dd6e10cad86a9110ed4b10c9df3077
Anuragp <anuragp@Anuragps-MacBook-Pro.local>
2020-01-29 14:33:03 +0530

    --- Auto-Git Commit ---

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -19,7 +19,7 @@ pipeline {
     
         stage("test") {
             steps {
-                sh ' echo "$hello pipelineParams.service" '
+                sh ' echo "$hello $pipelineParams.service" '
             }
     }
   }
