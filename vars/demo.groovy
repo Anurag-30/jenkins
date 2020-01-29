7b04d312f79cfd9f270ac35f5b24c4ca92f30edc
Anuragp <anuragp@Anuragps-MacBook-Pro.local>
2020-01-29 14:50:35 +0530

    --- Auto-Git Commit ---

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -8,7 +8,7 @@ pipeline {
     stages {
         stage("Env Variables") {
             steps {
-                sh "printenv "
+                sh "printenv | sort "
                 echo "${pipelineParams.service}"
                 sh " docker pull ${pipelineParams.service}:${env.GIT_COMMIT | cut -c1-5}"
             }
