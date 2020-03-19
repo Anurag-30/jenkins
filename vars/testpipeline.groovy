3c716caa3caa5f22038f88db90e90b0ee09a96a0
Anuragp <Anurag>
2020-03-19 12:06:40 +0530

    --- Auto-Git Commit ---

--- vars/testpipeline.groovy
+++ vars/testpipeline.groovy
@@ -2,7 +2,7 @@ def deleteExistingHelmChart() {
     container('jenkins-agent') {
         withFolderProperties {
             sh '''
-            helm repo add stable https://artifactory.dev.maximus.axisb.com/artifactory/forex-helm-virtual
+            helm repo add stable https://artifactory.test.cicd.com/artifactory/forex-helm-virtual
             if [ $(helm list -n ${TASK_NAMESPACE} | grep seed-${ENVIRONMENT_NAME} >/dev/null; echo $?) -eq 0 ];
             then
                 helm delete seed-${ENVIRONMENT_NAME} -n ${TASK_NAMESPACE};
@@ -17,12 +17,12 @@ def setSeedVersion(String upstreamEnv) {
         container('jenkins-agent') {
             script {
                 if("${ENVIRONMENT_NAME}" == "sit") {
-                    def app = sh(returnStdout: true, script: "curl -k 'https://artifactory.dev.maximus.axisb.com/artifactory/forex-generic/services-build-versions/seed/version.txt'")
+                    def app = sh(returnStdout: true, script: "curl -k 'https://artifactory.test.cicd.com/artifactory/forex-generic/services-build-versions/seed/version.txt'")
                     env.SEED_VERSION = app.split('=')[1]
                 }
                 else {
                     def upstream_build_number = UPSTREAM_BUILD.split('#')[1]
-                    def app = sh(returnStdout: true, script: "curl -k https://artifactory.dev.maximus.axisb.com/artifactory/forex-generic/${upstreamEnv}-verified/seed/version-${upstream_build_number}.txt")
+                    def app = sh(returnStdout: true, script: "curl -k https://artifactory.test.cicd.com/artifactory/forex-generic/${upstreamEnv}-verified/seed/version-${upstream_build_number}.txt")
                     env.SEED_VERSION = app.split('=')[1]
                 }
             }
@@ -35,7 +35,7 @@ def installHelmChart() {
         container('jenkins-agent') {
             sh '''
             echo "### Installing seed helm chart with version: ${SEED_VERSION}"
-            helm repo add stable https://artifactory.dev.maximus.axisb.com/artifactory/forex-helm-virtual
+            helm repo add stable https://artifactory.test.cicd.com/artifactory/forex-helm-virtual
             helm upgrade seed-${ENVIRONMENT_NAME} stable/seed  --install --version ${SEED_VERSION} --set environment.name=${ENVIRONMENT_NAME} --namespace ${TASK_NAMESPACE}
             '''
         }
@@ -49,8 +49,8 @@ def publishVersion() {
             echo "### Publishing seed helm chart version: ${SEED_VERSION} in file version-${BUILD_NUMBER}.txt"
             printf "seed_version=%s\n" ${SEED_VERSION} >> version-${BUILD_NUMBER}.txt
             cp version-${BUILD_NUMBER}.txt version-latest.txt
-            curl -k -u ${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW} -T version-latest.txt https://artifactory.dev.maximus.axisb.com/artifactory/forex-generic/${ENVIRONMENT_NAME}-verified/seed/version-latest.txt
-            curl -k -u ${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW} -T version-${BUILD_NUMBER}.txt https://artifactory.dev.maximus.axisb.com/artifactory/forex-generic/${ENVIRONMENT_NAME}-verified/seed/version-${BUILD_NUMBER}.txt
+            curl -k -u ${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW} -T version-latest.txt https://artifactory.test.cicd.com/artifactory/forex-generic/${ENVIRONMENT_NAME}-verified/seed/version-latest.txt
+            curl -k -u ${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW} -T version-${BUILD_NUMBER}.txt https://artifactory.test.cicd.com/artifactory/forex-generic/${ENVIRONMENT_NAME}-verified/seed/version-${BUILD_NUMBER}.txt
             '''
         }
     }
