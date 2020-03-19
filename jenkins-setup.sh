4fc8b0f467c70cff618647f46f585e6b0b057692
Anuragp <Anurag>
2020-03-19 11:00:17 +0530

    --- Auto-Git Commit ---

--- jenkins-setup.sh
+++ jenkins-setup.sh
@@ -4,6 +4,7 @@ kubectl create ns ci-cd
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 helm install jenkins stable/jenkins -f jenkins-values.yml --namespace ci-cd
+kubectl apply -f 
 
 
 
