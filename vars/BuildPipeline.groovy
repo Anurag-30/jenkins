c7c234196f7d3a56ff31f0c6653aec82f6a46f9c
Anuragp <Anurag>
2020-03-19 11:57:09 +0530

    --- Auto-Git Commit ---

---
+++ vars/BuildPipeline.groovy
@@ -0,0 +1,86 @@
+def call(Map pipelineParams) {
+    def git_version_script = libraryResource 'git_version.sh'
+    pipeline {
+        agent {
+            kubernetes {
+                yaml libraryResource('jenkins-agent-pod.yaml')
+            }
+        }
+
+        environment {
+            ARTIFACTORY_CREDENTIALS = credentials('jfrog-user')
+            GIT_AUTH = credentials('GIT_AUTH')
+        }
+
+        stages {
+            stage('build app') {
+                steps {
+                    container('jenkins-agent') {
+                        sh "gradle clean build docker --stacktrace"
+                        writeFile file: 'git_version.sh', text: "${git_version_script}"
+                        sh "git config --global http.postBuffer 524288000"
+                        sh "sh git_version.sh -b gradle"
+                        script {
+                            env.Version = sh(returnStdout: true, script: 'sh git_version.sh -v gradle').trim()
+                        }
+                    }
+                }
+            }
+
+            stage('build image and push') {
+                steps {
+                    container('jenkins-agent') {
+                        echo "Version is ${env.Version}"
+                        sh "docker login artifactory.dev.maximus.axisb.com -u=${ARTIFACTORY_CREDENTIALS_USR} -p=${ARTIFACTORY_CREDENTIALS_PSW}"
+                        sh "docker push artifactory.dev.maximus.axisb.com/forex-docker/${env.SERVICE}:latest"
+                        sh "docker tag artifactory.dev.maximus.axisb.com/forex-docker/${env.SERVICE}:latest artifactory.dev.maximus.axisb.com/forex-docker/${env.SERVICE}:${env.Version}"
+                        sh "docker push artifactory.dev.maximus.axisb.com/forex-docker/${env.SERVICE}:${env.Version}"
+                    }
+                }
+            }
+
+            stage('Helm package and push') {
+                steps {
+                    container('jenkins-agent') {
+                        sh "sed -ri \"s/tag: latest/tag: ${env.Version}/\" helm/${env.SERVICE}/values.yaml"
+                        sh "helm lint helm/${env.SERVICE}"
+                        sh "helm package helm/${env.SERVICE} --version ${env.Version}"
+                        sh "helm repo add stable https://artifactory.dev.maximus.axisb.com/artifactory/forex-helm-virtual"
+                        sh 'curl -k -u ${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW} -T ${SERVICE}-${Version}.tgz https://artifactory.dev.maximus.axisb.com/artifactory/forex-helm/maverick/${SERVICE}/${SERVICE}-${Version}.tgz '
+                    }
+                }
+            }
+
+            stage('push service version') {
+                steps {
+                    container('jenkins-agent') {
+                        sh "echo ${env.SERVICE}=${env.Version} >> version.txt"
+                        sh 'curl -k -u ${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW} -T version.txt https://artifactory.dev.maximus.axisb.com/artifactory/forex-generic/services-build-versions/${SERVICE}/version.txt'
+                    }
+                }
+            }
+
+            stage('trigger generate_build_versions job') {
+                steps{
+                    container('jenkins-agent') {
+                        build (job: 'Generate_Build_Version', propagate: false, wait: false)
+                    }
+                }
+            }
+        }
+
+        post {
+            always {
+                // publish test reports
+                publishHTML target:[
+                    allowMissing: false,
+                    alwaysLinkToLastBuild: false,
+                    keepAll: true,
+                    reportDir: 'build/reports/tests/test',
+                    reportFiles: 'index.html',
+                    reportName: "Tests Report"
+                ]
+            }
+        }
+    }
+}
