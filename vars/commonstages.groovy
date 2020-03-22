c434d110429297ce5fcd2e1eab1b893b7f29c49f
Anuragp <Anurag>
2020-03-22 12:19:18 +0530

    --- Auto-Git Commit ---

--- vars/commonstages.groovy
+++ vars/commonstages.groovy
@@ -69,10 +69,10 @@ def triggerDownstreamservice(String downstreamEnv) {
 def PublishArtifacts() {
     container('jenkins-agent') {
             post {
-        always {
-            archiveArtifacts artifacts: 'smoke-test/cypress/screenshots/**/*.*', fingerprint: true, allowEmptyArchive: true
-            archiveArtifacts artifacts: 'smoke-test/TestsReports/*.*', fingerprint: true, allowEmptyArchive: true
-            archiveArtifacts artifacts: 'helm-charts/application-services/requirements.yaml', fingerprint: true, allowEmptyArchive: true
+             always {
+                archiveArtifacts artifacts: 'smoke-test/cypress/screenshots/**/*.*', fingerprint: true, allowEmptyArchive: true
+                archiveArtifacts artifacts: 'smoke-test/TestsReports/*.*', fingerprint: true, allowEmptyArchive: true
+                archiveArtifacts artifacts: 'helm-charts/application-services/requirements.yaml', fingerprint: true, allowEmptyArchive: true
                 }
     }
 }
