a0a135e41d0d1d2f65c01e6993657c3dffa6b640
Anuragp <Anurag>
2020-03-22 11:32:46 +0530

    --- Auto-Git Commit ---

--- vars/BuildPipeline.groovy
+++ vars/BuildPipeline.groovy
@@ -3,7 +3,7 @@ def call(Map pipelineParams) {
     pipeline {
         agent {
             kubernetes {
-                yaml libraryResource('jenkins-agent-pod.yaml') // You will find all the 
+                yaml libraryResource('jenkins-agent-pod.yaml') // You will find all the common files under resources
             }
         }
 
