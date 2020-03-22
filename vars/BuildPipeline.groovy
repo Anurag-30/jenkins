672534e0a04aa7c276bd42f79ca7acc672e7b428
Anuragp <Anurag>
2020-03-22 12:49:22 +0530

    --- Auto-Git Commit ---

--- vars/BuildPipeline.groovy
+++ vars/BuildPipeline.groovy
@@ -46,7 +46,7 @@ def call(Map pipelineParams) {
                         sh "helm lint helm/${env.SERVICE}"
                         sh "helm package helm/${env.SERVICE} --version ${env.Version}"
                         sh "helm repo add stable https://artifactory.test.cicd.com/artifactory/anurag-helm-virtual"
-                        sh 'curl -k -u ${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW} -T ${SERVICE}-${Version}.tgz https://artifactory.test.cicd.com/artifactory/anurag-helm/maverick/${SERVICE}/${SERVICE}-${Version}.tgz '
+                        sh 'curl -k -u ${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW} -T ${SERVICE}-${Version}.tgz https://artifactory.test.cicd.com/artifactory/anurag-helm/Application/${SERVICE}/${SERVICE}-${Version}.tgz '
                     }
                 }
             }
